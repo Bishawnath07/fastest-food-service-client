bec5958847d39b77c2ccf9076bca16a64b13a3f8
Bishawnath Ray <bishawnathprogram07@gmail.com>
2023-05-03 21:26:24 +0600

    added chefs recipe

--- src/Pages/chefDetails/ChefDetails.jsx
+++ src/Pages/chefDetails/ChefDetails.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import { FaEye } from 'react-icons/fa';
+import { FaEye, FaRegThumbsUp } from 'react-icons/fa';
 import { useLoaderData } from 'react-router-dom';
+import ChefCard from './ChefCard/ChefCard';
 
 const ChefDetails = () => {
     const chefs = useLoaderData();
     console.log(chefs);
-    const { id, name, img, years_of_experience, number_of_recipes, likes } = chefs ;
+    const { id, name, items , description, img, years_of_experience, number_of_recipes, likes } = chefs;
 
 
 
     return (
-        <div className="card card-side bg-base-100 shadow-xl ms-20">
-            <figure><img className='h-80 w-72' src={img} alt="Movie" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">{name} </h2>
-                <p>Click the button to watch on Jetflix app.</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Faveriot</button>
+        <div className=' mx-20  mt-16'>
+            <div className="card card-side bg-base-100 shadow-xl p-10 my-10">
+                <figure>
+                    <img className='image-full' src={img} alt="Movie" />
+                </figure>
+                <div className="card-body ">
+                    <h2 className="card-title text-3xl font-bold"> Name: {name} </h2>
+                    <p className='font-semibold'>{description}</p>
+                    <p className='font-semibold'>Years of experience : {years_of_experience}</p>
+                    <p className='font-semibold mt-0'>Number of recupes : {number_of_recipes}</p>
+                    <p className='flex items-center gap-1'>Likes :<FaRegThumbsUp className='text-red-500'></FaRegThumbsUp> {likes}</p>
+                    
                 </div>
             </div>
+            <h1 className='text-5xl text-center my-10'>Here are some recipe </h1>
+            <ChefCard></ChefCard>
+          
         </div>
     );
 };
